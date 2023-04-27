45773746525b2be79b78056cd63a71be4028d996
isydorenk0 <iurii.sydroenko@gmailcom>
2023-04-27 14:57:22 -0400

    Initial commit

---
+++ src/Ex2/Decrypt.java
@@ -0,0 +1,65 @@
+package Ex2;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Decrypt {
+    public static void main(String[] args) throws Exception {
+        File file = new File("exercise2.txt");
+        createRewriteFile(file);
+        String data = getTextFromFile(file);
+        String preposition = getTextFromFile(new File("preposition.txt"));
+        String result = Pattern.compile(preposition, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE).matcher(data).replaceAll(Matcher.quoteReplacement("Java"));
+        saveToFile(new File("decrypted.txt"), result);
+    }
+
+    private static String getTextFromFile(File file) throws Exception {
+        StringBuilder string = new StringBuilder();
+        try {
+            if (file.getName().equals("preposition.txt")) {
+                Files.lines(file.toPath()).forEach(s -> string.append("\\b(").append(s).append(")\\b|"));
+            } else {
+                Files.lines(file.toPath()).forEach(s -> string.append(s).append("\n"));
+            }
+        } catch (Exception e) {
+            throw new Exception(Arrays.toString(e.getStackTrace()));
+        }
+        string.deleteCharAt(string.length() - 1);
+        return string.toString();
+    }
+
+    private static void createRewriteFile(File file) throws Exception {
+        URL url = new URL("https://www.gutenberg.org/ebooks/1513.txt.utf-8");
+        InputStream is = null;
+        try {
+            URLConnection con = url.openConnection();
+            is = con.getInputStream();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+        saveToFile(file, is);
+    }
+
+    private static void saveToFile(File file, InputStream is) throws Exception {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(file));
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                pw.write(line + "\n");
+            }
+            pw.flush();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    private static void saveToFile(File file, String data) throws Exception {
+        Files.write(Paths.get(file.getPath()), data.getBytes());
+    }
+}
---
+++ src/Ex3/Main.java
@@ -0,0 +1,55 @@
+package Ex3;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        LocalDateTime date = getDate(scanner);
+        calcTodayDif(date);
+    }
+
+    private static void calcTodayDif(LocalDateTime date) {
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println("From date: " + date);
+        System.out.println("To date: " + today);
+
+        if (today.isBefore(date)) {
+            System.out.println("Entered date is in future");
+            return;
+        } else if (today.isEqual(date)) {
+            System.out.println("Entered date is today");
+            return;
+        }
+        long diffMs = ChronoUnit.MILLIS.between(date, today);
+        long diffSec = (diffMs / 1000) % 60;
+        long diffMin = (diffMs / (60 * 1000)) % 60;
+        long diffHrs = (diffMs / (60 * 60 * 1000)) % 24;
+        // використовую Період, щоб самому не рахувати кількість днів і місяців
+        Period diffDate = Period.between(date.toLocalDate(), today.toLocalDate());
+
+        System.out.println("You are " + diffDate.getYears() + " years, " + diffDate.getMonths() +
+                " months, " + diffDate.getDays() + " days, " + diffHrs + " hours, " + diffMin + " minutes, and " +
+                diffSec + " seconds old.");
+    }
+
+    private static LocalDateTime getDate(Scanner scanner) {
+        while(true) {
+            System.out.println("Enter date and time, in format dd/mm/yyyy hh:mm:ss: ");
+            String number = scanner.nextLine();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+            try {
+                return sdf.parse(number).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            } catch (ParseException e) {
+                System.out.println("Entered date has wrong format");
+            }
+        }
+    }
+}
